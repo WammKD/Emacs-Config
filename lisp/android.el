a9a74fdb6b57c57605cd898ccd6b9b152231cb25
WammKD <jaft.r@outlook.com>
2018-01-21 14:01:28 -0600

    Made the keystore function work better and some new packages.

--- lisp/android.el
+++ lisp/android.el
@@ -3,20 +3,21 @@
 
   (defun get-passwords (count)
     (setq firstPassword    (if (> count 0)
-                               (read-passwd
-                                 (string-join (list
-                                                "Passwords didn't match! "
-                                                "Enter keystore password: ")))
+                               (read-passwd (concat
+                                              "Passwords didn't match! "
+                                              "Enter keystore password: "))
                              (read-passwd "Enter keystore password: "))
           repeatedPassword (read-passwd "Re-enter new password: ")
           countPlusOne     (1+ count))
 
     (cond
-     ((= countPlusOne 3)                                              nil)
+     ((= countPlusOne 3)                                            nil)
      ((string-equal
         firstPassword
-        repeatedPassword) (list firstPassword "\n" repeatedPassword "\n"))
-     (t                                      (get-passwords countPlusOne))))
+        repeatedPassword) (cons firstPassword (concat
+                                                firstPassword    "\n"
+                                                repeatedPassword "\n")))
+     (t                                    (get-passwords countPlusOne))))
   (defun get-rest (a p)
     (setq name      (read-string "What is your first and last name? ")
           orgUnit   (read-string
@@ -27,27 +28,20 @@
           twoLetter (read-string
                       "What is the two-letter country code for this unit? "))
 
-    (if (yes-or-no-p (string-join (list "Is CN=" name
-                                        ", OU="  orgUnit
-                                        ", O="   org
-                                        ", L="   city
-                                        ", ST="  state
-                                        ", C="   twoLetter " correct? ")))
-        (list
-          name      "\n"
-          orgUnit   "\n"
-          org       "\n"
-          city      "\n"
-          state     "\n"
-          twoLetter "\nyes\n"
-          (read-string
-            (string-join (list
-                           "Enter key password for <"
-                           a
-                           "> (RETURN if same as keystore password): "))
-            nil
-            nil
-            p)      "\n")
+    (if (yes-or-no-p (concat "Is CN=" name  ", OU="  orgUnit
+                             ", O="   org   ", L="   city
+                             ", ST="  state ", C="   twoLetter " correct? "))
+        (concat name  "\n" orgUnit   "\n"
+                org   "\n" city      "\n"
+                state "\n" twoLetter "\nyes\n"
+                (read-string
+                  (concat
+                    "Enter key password for <"
+                    a
+                    "> (RETURN if same as keystore password): ")
+                  nil
+                  nil
+                  p)  "\n")
       (get-rest a p)))
 
   (setq filePath   (read-directory-name "Directory to create keystore in: ")
@@ -58,33 +52,25 @@
                      "release.keystore")
         alias      (read-string "Keystore alias (\"example\"): " nil
                                 nil                              "example")
-        keytoolCmd (string-join (list
-                                  " | keytool -genkey -v -keystore "
-                                  fileName
-                                  " -alias "
-                                  alias
-                                  " -keyalg RSA -keysize "
-                                  (read-string
-                                    "Keystore size (\"2048\"): "
-                                    nil
-                                    nil
-                                    "2048")
-                                  " -validity "
-                                  (read-string
-                                    "Keystore validity (\"10000\"): "
-                                    nil
-                                    nil
-                                    "10000")
-                                  " -deststoretype pkcs12"))
+        keytoolCmd (concat
+                     " | keytool -genkey -v -keystore "
+                     fileName
+                     " -alias "
+                     alias
+                     " -keyalg RSA -keysize "
+                     (read-string "Keystore size (\"2048\"): " nil nil "2048")
+                     " -validity "
+                     (read-string "Keystore validity (\"10000\"): " nil
+                                  nil                               "10000")
+                     " -deststoretype pkcs12")
         passwords  (get-passwords 0))
 
   (if passwords
       (progn
-        (setq command (string-join (append
-                                     (list "cd " filePath "; printf \"")
-                                     passwords
-                                     (get-rest alias (car passwords))
-                                     (list "\"" keytoolCmd))))
+        (setq command (concat "cd "         filePath
+                              "; printf \"" (cdr passwords)
+                              (get-rest alias (car passwords))
+                              "\""          keytoolCmd))
 
         (split-window-vertically (- (window-height) 12))
         (set-window-buffer (next-window) (get-buffer-create
