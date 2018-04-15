4e5297823469590abbe614eb6750c68d8900382c
WammKD <jaft.r@outlook.com>
2018-04-14 23:56:17 -0500

    Add the ability to rename an open file.

--- init.el
+++ init.el
@@ -633,6 +633,24 @@ mouse-3: Next buffer")
 (add-to-list 'tramp-remote-path                "/system/xbin")
 (add-to-list 'tramp-remote-process-environment "TMPDIR=$HOME")
 
+;; Buffer Shit
+  ; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name for file: ")
+  (let ((name          (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+(global-set-key (kbd "C-x M-w") 'rename-file-and-buffer)
+
 ;; Coding Shit?
 (require 'smart-tabs-mode)
 
