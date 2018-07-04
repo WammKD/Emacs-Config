16ea3d6ef95be21bc29deb33d8d432f207582017
Wamm K. D <jaft.r@outlook.com>
2018-07-04 00:12:25 -0500

    New way of doing things for Package and a space to make things easier to read.

--- init.el
+++ init.el
@@ -1,7 +1,14 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;; Loadpaths
 (add-to-list 'load-path              "~/.emacs.d/lisp")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-klere-theme")
+
 
 ;; Packages
 (require 'package)
