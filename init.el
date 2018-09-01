18a365cf3d8ee586e687926cce0c22a5e0d8d017
Wamm K. D <jaft.r@outlook.com>
2018-09-01 01:41:19 -0500

    Setup `company` for `robe`; `autocomplete` isn't showing documentation, for some reason, so using `company`, instead. I'll probably do the same for `inf-ruby`, as well…

--- init.el
+++ init.el
@@ -918,7 +918,6 @@ prefer for `sh-mode'.  It is automatically added to
 
   ;; Ruby Shit
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-;; (add-hook    'enh-ruby-mode-hook 'ac-robe-setup)
 
 (add-hook 'enh-ruby-mode-hook (lambda ()
                                 (setq ruby-indent-level 2
@@ -928,6 +927,14 @@ prefer for `sh-mode'.  It is automatically added to
 
 ; flymake
 (add-hook 'enh-ruby-mode-hook 'flymake-ruby-load)
+
+; robe
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+(add-hook 'enh-ruby-mode-hook 'company-mode)
+;; (add-hook 'enh-ruby-mode-hook 'auto-complete-mode)
+;; (add-hook 'robe-mode-hook     'ac-robe-setup)
 
 ; inf settings
 (eval-after-load 'auto-complete '(add-to-list 'ac-modes 'inf-ruby-mode))
@@ -1335,7 +1342,7 @@ prefer for `sh-mode'.  It is automatically added to
  '(geiser-guile-extra-keywords (quote ("return-if" "if-let")))
  '(package-selected-packages
    (quote
-    (ac-inf-ruby inf-ruby flymake-ruby markdown-mode racer rust-mode racket-mode scheme-complete dictionary meghanada bongo ivy-youtube ceylon-mode javadoc-lookup enh-ruby-mode lua-mode web-mode qml-mode auctex ac-geiser geiser all-the-icons company magit))))
+    (robe ac-inf-ruby inf-ruby flymake-ruby markdown-mode racer rust-mode racket-mode scheme-complete dictionary meghanada bongo ivy-youtube ceylon-mode javadoc-lookup enh-ruby-mode lua-mode web-mode qml-mode auctex ac-geiser geiser all-the-icons company magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
