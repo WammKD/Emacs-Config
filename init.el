f0c0eb917682073b9915542c5e5b8d6039fc9c78
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2021-03-20 11:55:48 -0500

    I…don't remember what these are. But probably useful.

--- init.el
+++ init.el
@@ -967,9 +967,11 @@ This is handy if you're not yet using `paredit-mode',
          (close-char (and close-pair   (cdr close-pair))))
     (guile--self-insert (or close-char last-command-event))))
 
-(put 'if      'scheme-indent-function 2)
-(put 'if-let  'scheme-indent-function 2)
-(put 'if-let* 'scheme-indent-function 2)
+(put 'if         'scheme-indent-function 2)
+(put 'if-let     'scheme-indent-function 2)
+(put 'if-let*    'scheme-indent-function 2)
+(put 'case-pred  'scheme-indent-function 2)
+(put 'stream-let 'scheme-indent-function 2)
 
 (setq scheme-program-name   "guile")
 (setq geiser-mode-company-p nil)
